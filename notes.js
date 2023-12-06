7121bf8baa84dc01ea5abc9c1e45614f77dc05aa
Kirtika Gautam <100124517+Kirtikaa25@users.noreply.github.com>
2023-12-07 00:34:37 +0530

    Create notes.js

---
+++ notes.js
@@ -0,0 +1,8 @@
+const notes = [
+  {
+    key: 1,
+    title: "Delegation",
+    content:
+      "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem"
+  }
+]
