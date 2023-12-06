40d113b25e89c382fe840f8bf7429b3fd8f6fb45
Kirtika Gautam <100124517+Kirtikaa25@users.noreply.github.com>
2023-12-07 00:46:23 +0530

    Update App1.jsx

--- App1.jsx
+++ App1.jsx
@@ -12,4 +12,22 @@ function App(){
         </div>
     )
 }
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(noteItem => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
 export default App;
+
+
