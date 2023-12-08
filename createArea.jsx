c148183c491dee714b710e7e2fdda70be7b5e4d4
Kirtika Gautam <100124517+Kirtikaa25@users.noreply.github.com>
2023-12-09 01:40:24 +0530

    Update createArea.jsx

--- createArea.jsx
+++ createArea.jsx
@@ -2,3 +2,10 @@ import React, { useState } from "react";
 import AddIcon from "@material-ui/icons/Add";
 import Fab from "@material-ui/core/Fab";
 
+function CreateArea(props) {
+  const [isExpanded, setExpanded] = useState(false);
+
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
