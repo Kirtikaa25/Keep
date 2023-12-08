0241a66de6b3e8175ac0f7d842b2b211f61ce436
Kirtika Gautam <100124517+Kirtikaa25@users.noreply.github.com>
2023-12-09 01:37:06 +0530

    Update App1.jsx

--- App1.jsx
+++ App1.jsx
@@ -1,23 +1,7 @@
 
 
 
-function App() {
-  return (
-    <div>
-      <Header />
-      {notes.map(noteItem => (
-        <Note
-          key={noteItem.key}
-          title={noteItem.title}
-          content={noteItem.content}
-        />
-      ))}
-      <Footer />
-    </div>
-  );
-}
 
-export default App;
 import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
