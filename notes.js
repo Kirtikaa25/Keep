9f5568c117ade9f1f8ba1adc9daad0bedab15aa6
Kirtika Gautam <100124517+Kirtikaa25@users.noreply.github.com>
2023-12-23 20:01:33 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -1,7 +1,7 @@
 const notes = [
   {
     key: 1,
-    title: "Delegation",
+    title: "Delegation"
     content:
       "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem"
   },
